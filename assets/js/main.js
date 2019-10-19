3c29a6dd26f352a8c187725ffdf80d6c80ab4e9e
2markit <2markit.consulting@gmail.com>
2019-10-19 14:42:02 -0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Verti by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -37,6 +31,7 @@
 			$(
 				'<div id="navToggle">' +
 					'<a href="#navPanel" class="toggle"></a>' +
+					'<a href="#" class="btn"></a>' +
 				'</div>'
 			)
 				.appendTo($body);
