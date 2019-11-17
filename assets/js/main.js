bc3e4f38ec46cc55b798246b9534009bf38620d6
2markit <2markit.consulting@gmail.com>
2019-11-16 20:00:27 -0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@
 				$(document).ready(function () {
 				    // Handler for .ready() called.
 				    $('html, body').animate({
-				        scrollTop: $('#top').offset().top - 125
+				        scrollTop: $('#top').offset() - 125
 				    },'slow');
 				});
 
